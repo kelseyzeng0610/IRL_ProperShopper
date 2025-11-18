a9730966f0a2af7df131b608eb191cc2a3a4560e
Michael Behrens <behrensm03@gmail.com>
2025-11-17 23:47:06 -0500

    fix: tweaks to max entropy

--- max_entropy_irl.py
+++ max_entropy_irl.py
@@ -4,14 +4,6 @@ def softmax(vect):
     e_x = np.exp(vect - np.max(vect))
     return e_x / np.sum(e_x)
 
-def feature_count(trajectory):
-    f = [s for (s, a, sprime) in trajectory]
-    return np.sum(f, axis=0) / len(trajectory)
-
-def average_feature_counts(trajectories):
-    total = sum(feature_count(traj) for traj in trajectories)
-    return total / len(trajectories)
-
 def writeProgressBar(i, n):
     progress = i / n
     barLength = 40
@@ -19,47 +11,54 @@ def writeProgressBar(i, n):
     print(f"\r[{bar}] {progress*100:5.1f}% ", end="", flush=True)
 
 class MaxEntropyIRL:
-    def __init__(self, theta, actions, probeNextState, initialState, gameOver, phi, maxTrajectoryLength=50):
+    def __init__(self, theta, actions, probeNextState, initialState, gameOver, phi):
         self.theta = theta
         self.actions = actions
         self.probeNextState = probeNextState
         self.initialState = initialState
-        self.maxTrajectoryLength = maxTrajectoryLength
         self.gameOver = gameOver
         self.phi = phi
 
     def reward(self, state):
-        return np.dot(self.theta, state)
+        return np.dot(self.theta, self.phi(state))
 
     def stochastic_trajectory(self, maxLength=None):
         currentState = self.initialState
-        trajectory = []
+        trajectory = [currentState]
         while not self.gameOver(currentState) and (maxLength is None or len(trajectory) < maxLength):
             q_values = []
             next_states = []
             for a in self.actions:
-                ns = self.probeNextState(currentState, a)
+                ns = np.asarray(self.probeNextState(currentState, a))
                 next_states.append(ns)
                 q_values.append(self.reward(ns))
-            prob_values = softmax(np.array(q_values))
 
+            prob_values = softmax(np.array(q_values))
             randomIdx = np.random.choice(len(next_states), p=prob_values)
             nextState = next_states[randomIdx]
-            trajectory.append([currentState, randomIdx, nextState])
+
+            trajectory.append(nextState)
             currentState = nextState
 
         return trajectory
     
+    def feature_count(self, trajectory):
+        features = [self.phi(step) for step in trajectory]
+        return np.sum(features, axis=0) / len(trajectory)
 
-    def learn(self, expert_trajectories, num_iterations=200, num_samples=50, alpha = 0.01):
-        muhat_expert = average_feature_counts(expert_trajectories)
-        grad_norms, thetas = [], []
+    def average_feature_counts(self, trajectories):
+        total = sum(self.feature_count(traj) for traj in trajectories)
+        return total / len(trajectories)
+    
+
+    def learn(self, expert_trajectories, num_iterations=200, num_samples=50, alpha = 0.01, maxTrajectoryLength=50):
+        muhat_expert = self.average_feature_counts(expert_trajectories)
+        grad_norms = []
         for iteration in range(num_iterations):
-            thetas.append(self.theta)
             # First sample some trajectories according to our current reward estimate (theta)
-            sample_trajectories = [self.stochastic_trajectory(maxLength=self.maxTrajectoryLength) for m in range(num_samples)]
+            sample_trajectories = [self.stochastic_trajectory(maxLength=maxTrajectoryLength) for _ in range(num_samples)]
             # Get the average feature counts according to our model
-            muhat = average_feature_counts(sample_trajectories)
+            muhat = self.average_feature_counts(sample_trajectories)
             # Update step: expert minus model
             grad = muhat_expert - muhat
             self.theta += alpha * grad
@@ -67,4 +66,5 @@ class MaxEntropyIRL:
             writeProgressBar(iteration+1, num_iterations)
             
 
-        return self.theta, grad_norms, thetas
+        return self.theta, grad_norms
+
--- maxent_test.py
+++ maxent_test.py
@@ -28,18 +28,86 @@ def plot_grad_norms(grad_norms):
     plt.show()
 
 if __name__ == "__main__":
-    expertTrajectories = [
-        [(0,0), (1,0), (2,0), (3,0), (4,0), (4,1), (4,2), (4,3), (4,4)],
-        [(0,0), (0,1), (0,2), (0,3), (0,4), (1,4), (2,4), (3,4), (4,4)],
-        [(0,0), (1,0), (1,1), (2,1), (2,2), (3,2), (3,3), (4,3), (4,4)]
-    ]
 
-    np.random.seed(42)
-    theta_random = np.random.uniform(low=0.0, high=0.1, size=2)
 
-    learner = MaxEntropyIRL(theta=theta_random, actions=ACTIONS, getNextState=getNextState)
-    theta_hat, grad_norms = learner.learn(expertTrajectories)
-    print(theta_hat)
-    sampleTrajectory = learner.stochastic_trajectory()
-    print(sampleTrajectory)
-    plot_grad_norms(grad_norms)
+    # BASIC TEST 1:
+    # starting from 0,0, navigate to 4,4
+    def basicTest1():
+        expertTrajectories = [
+            [(0,0), (1,0), (2,0), (3,0), (4,0), (4,1), (4,2), (4,3), (4,4)],
+            [(0,0), (0,1), (0,2), (0,3), (0,4), (1,4), (2,4), (3,4), (4,4)],
+            [(0,0), (1,0), (1,1), (2,1), (2,2), (3,2), (3,3), (4,3), (4,4)]
+        ]
+        def phi(state):
+            return np.asarray(state) # for now phi is just x,y
+        def gameOver(state):
+            return np.allclose(state, np.asarray([4,4]))
+
+        np.random.seed(42)
+        theta_random = np.random.uniform(low=0.0, high=0.1, size=2)
+
+        learner = MaxEntropyIRL(
+            theta=theta_random, 
+            actions=ACTIONS, 
+            probeNextState=getNextState,
+            initialState=(0,0),
+            gameOver=gameOver,
+            phi=phi,
+        )
+        theta_hat, _ = learner.learn(expertTrajectories)
+        assert theta_hat[0] > 0
+        assert theta_hat[1] > 0
+
+        print("Pass basic test 1: both theta values are positive")
+        sampleTrajectory = learner.stochastic_trajectory()
+        x, y = [step[0] for step in sampleTrajectory], [step[1] for step in sampleTrajectory]
+        plt.plot(x, y, 'o-', color='blue', label='Sampled trajectory')
+        plt.grid(True)
+        plt.legend()
+        plt.show()
+
+    # BASIC TEST 2:
+    # Navigate from 4,0 to 2,4 - this requires us to change phi
+    def basicTest2():
+        expertTrajectories = [
+            [(4,0), (3,0), (2,0), (2,1), (2,2), (2,3), (2,4)],
+            [(4,0), (4,1), (4,2), (4,3), (4,4), (3,4), (2,4)],
+            [(4,0), (3,0), (3,1), (3,2), (3,3), (2,3), (2,4)],
+        ]
+       
+        def getphi(goal):
+            def phi(state):
+                delta = [goal[0] - state[0], goal[1] - state[1]]
+                return np.asarray(delta) # now phi is a vector pointing to the goal
+            return phi
+        def gameOver(state):
+            return np.allclose(state, np.asarray([2,4]))
+
+        np.random.seed(42)
+        theta_random = np.random.uniform(low=0.0, high=0.1, size=2)
+
+        learner = MaxEntropyIRL(
+            theta=theta_random, 
+            actions=ACTIONS, 
+            probeNextState=getNextState,
+            initialState=(4,0),
+            gameOver=gameOver,
+            phi=getphi((2,4)),
+        )
+        theta_hat, _ = learner.learn(expertTrajectories)
+
+        # Now that phi is a vector, if theta is negative then that means we are learning to make the vector 0 -> that means we are approaching the goal location
+        assert theta_hat[0] < 0
+        assert theta_hat[1] < 0
+
+        print("Pass basic test 2: both theta values are negative")
+        sampleTrajectory = learner.stochastic_trajectory(maxLength=100)
+        x, y = [step[0] for step in sampleTrajectory], [step[1] for step in sampleTrajectory]
+        plt.plot(x, y, 'o-', color='blue', label='Sampled trajectory')
+        plt.grid(True)
+        plt.legend()
+        plt.show() 
+
+    basicTest1()
+    basicTest2()
+
