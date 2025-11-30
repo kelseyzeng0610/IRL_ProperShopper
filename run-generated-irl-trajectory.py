2bae2255ec10d97b341316c51e1fd83cd6df31b5
Michael Behrens <behrensm03@gmail.com>
2025-11-29 20:39:36 -0500

    feat: get basket on the way to sausage

--- run-generated-irl-trajectory.py
+++ run-generated-irl-trajectory.py
@@ -25,13 +25,39 @@ if __name__ == "__main__":
         0: 'NORTH',
         1: 'SOUTH',
         2: 'EAST',
-        3: 'WEST'
+        3: 'WEST',
+        4: "INTERACT",
     }
-    sock_game.send(str.encode("0 RESET"))
-    recv_socket_data(sock_game)
-
-    for action in generated_actions:
+    
+    # wrapper around the actual action execution to get rid of some of the issues.
+    # removes the turning aspect and always executes the movement actions directly.
+    # for interact actions, executes it twice to ensure stability.
+    def execute_action_with_turning(action, current_state):
+        current_direction = current_state['observation']['players'][0]['direction']
+        
+        # if movement is different than current direction, turn first
+        if action in [0, 1, 2, 3] and current_direction != action:
+            turn_command = f"0 {action_commands[action]}"
+            sock_game.send(str.encode(turn_command))
+            actual_state = recv_socket_data(sock_game)
+            actual_state = json.loads(actual_state)
+        
+        # TODO: there have been issues in the past with interact actions sometimes failing, so at some point
+        # we may want to replace every INTERACT with two INTERACTs here for stability.
+        # for now this seems to be ok.
+        
+        # the real action
         command = f"0 {action_commands[action]}"
         sock_game.send(str.encode(command))
-        recv_socket_data(sock_game)
+        actual_state = recv_socket_data(sock_game)
+        actual_state = json.loads(actual_state)
         time.sleep(0.1)
+        
+        return actual_state
+    
+    sock_game.send(str.encode("0 RESET"))
+    start_state = recv_socket_data(sock_game)
+    current_state = json.loads(start_state)
+
+    for action in generated_actions:
+        current_state = execute_action_with_turning(action, current_state)
